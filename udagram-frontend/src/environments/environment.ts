f5f567140b521f98794ebd19dab6ceb5c29d0818
Xuyen Nguyen <myxuyen0889@gmail.com>
2023-08-05 23:32:40 +0700

    update localhost

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ac5e1ed854a894872b81a5c8a4180639-2126541653.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
